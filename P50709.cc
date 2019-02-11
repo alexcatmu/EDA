2c9f6c87c377201ba5aebf8e8a1b8411240d5620
Alex Catalan Munoz <alex.catalan@a5s102pc43.fib.upc.es>
2019-02-11 09:07:24 +0100

    Collection of numbers

--- P50709.cc
+++ P50709.cc
@@ -1,1 +1,54 @@
- 
+#include <iostream>
+#include <queue>
+using namespace std; 
+
+int main(){
+    std::priority_queue<int> q;
+    char c;
+    int n;
+    int aux;
+    while (cin >> c){
+        switch(c){
+            case 'S':
+                cin >> n;
+                q.push(n);
+                break;
+                
+            case 'A':
+                if(q.empty()) cout << "error!" << endl;
+                else {
+                    cout << q.top() << endl;
+                }
+                break;
+                
+            case 'R':
+                if(q.empty()) cout << "error!" << endl;
+                else {
+                    q.pop();
+                }
+                break;
+                
+            case 'I':
+                cin >> n;
+                if(q.empty()) cout << "error!" << endl;
+                else {
+                    aux = q.top();
+                    aux += n;
+                    q.pop();
+                    q.push(aux);
+                }
+                break;
+                
+            case 'D':
+                cin >> n;
+                if(q.empty()) cout << "error!" << endl;
+                else {
+                    aux = q.top();
+                    aux -= n;
+                    q.pop();
+                    q.push(aux);
+                }
+                break;
+        }
+    }
+}
