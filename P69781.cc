0160ee3b4d31e796170a7a4c0af38d78e2a18c65
Alex Catalan <alex.catalan.catalan@gmail.com>
2019-02-12 21:43:48 +0100

    collatz pseudo-sequences

--- P69781.cc
+++ P69781.cc
@@ -1,11 +1,25 @@
 #include <iostream>
-#include <set>
+#include <map>
 using namespace std;
 
 int main(){
     int x,y,n;
-    cin >> x >> y >> n;
-    set<int> set1;
-    set1.insert(10);
-    for (auto it = set1.begin(); it != set1.end(); ++it) cout << *it << endl;
+    
+    while (cin >> x >> y >> n){
+        map<int,int> serie;
+        int i = 1;
+        serie.insert(make_pair(n,i));
+        bool encontrado = false;
+        auto search = serie.begin();
+        while(n <= 100000000 && !encontrado){
+            i++;
+            if(n % 2 == 0) n = n/2 + x;
+            else n = 3*n + y;
+            search = serie.find(n);
+            if(search != serie.end()) encontrado = true;
+            else serie.insert(make_pair(n,i));
+        }
+        if(!encontrado) cout << n << endl;
+        else cout << (serie.size() - search->second + 1) << endl;   
+    }
 }
