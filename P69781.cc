0c12e908008382b9cb0cbbcda3ae96f1a0ec28fd
Alex Catalan <alex.catalan.catalan@gmail.com>
2019-02-12 20:44:13 +0100

    Partial commit of collatz

--- P69781.cc
+++ P69781.cc
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <set>
+using namespace std;
 
-using namespace std(){
+int main(){
     int x,y,n;
     cin >> x >> y >> n;
-    
+    set<int> set1;
+    set1.insert(10);
+    for (auto it = set1.begin(); it != set1.end(); ++it) cout << *it << endl;
 }
